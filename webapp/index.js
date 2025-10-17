b47d80ed00654dfd489607e4af85ded254fd9114
hugosique <otahugo02@gmail.com>
2025-10-17 18:35:50 -0300

    3. Controls

--- webapp/index.js
+++ webapp/index.js
@@ -1,4 +1,9 @@
-sap.ui.define([], () => {
+sap.ui.define([
+	"sap/m/Text"
+], (Text) => {
 	"use strict";
-	alert("UI5 is ready");
+
+	new Text({
+		text: "Hello UI5 Walkthrough!"
+	}).placeAt("content");
 });

161f6f0243caf023a4eea8f60cc9d37f870977fe
hugosique <otahugo02@gmail.com>
2025-10-21 20:39:11 -0300

    7. JSON Model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,27 @@
-sap.ui.define([
-   "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast"
-], (Controller, MessageToast) => {
-   "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  (Controller, MessageToast, JSONModel) => {
+    "use strict";
 
-   return Controller.extend("ui5.walkthrough.controller.App", {
+    return Controller.extend("ui5.walkthrough.controller.App", {
+      onInit() {
+        const onData = {
+          recipient: {
+            name: "World",
+          },
+        };
+
+        const model = new JSONModel(onData);
+        this.getView().setModel(model);
+      },
+
       onShowHello() {
-         MessageToast.show("Hello World")
-      }
-   });
-});
+        MessageToast.show("Hello World");
+      },
+    });
+  }
+);
